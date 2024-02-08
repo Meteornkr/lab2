f77c9ee73083153b8b9d5ce8333b4fd0c35c1294
Jan Michael B. Villeza <142873574+Meteornkr@users.noreply.github.com>
2024-02-08 16:06:48 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT 'id', 'name', 'email' FROM jbvilleza_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM jbvilleza_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["$id"]. " - Name: " . $row["$name"]. " email: " . $row["$email"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " email: " . $row["email"].  $row["website"].  $row["comment"].  $row["gender"]. "<br>";
   }
 } else {
   echo "0 results";
